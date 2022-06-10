8cea8b7f9da3a52ea4d70de42f738744a18ce80b
FDA <40368675+FD4A@users.noreply.github.com>
2022-06-11 00:34:18 +0300

    Update MAIN_rm_template.c
    поправилд форматирование через github

--- RM_template/MAIN_rm_template.c
+++ RM_template/MAIN_rm_template.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 	uint64_t interval_nsec = 20*1000*1000;
 	DataServers_t DataServers;
 
-    /*create dispatcher*/
+	/*create dispatcher*/
 	dispatch_t *dpp;
 	dpp = dispatch_create();
 	if(NULL==dpp)
@@ -24,13 +24,13 @@ int main(int argc, char *argv[])
 	char *p[]={"/dev/NOT1","/dev/NOT2"};
 	Source_Init(&DataServers.Source,dpp,p,1);
 
-    /*init resmgr_attr*/
+    	/*init resmgr_attr*/
 	resmgr_attr_t resmgr_attr;
 		memset(&resmgr_attr,0,sizeof(resmgr_attr_t));
 		resmgr_attr.msg_max_size = 1024;
 		resmgr_attr.nparts_max = 1;
 
-    /*init tables of connection and input-output functions*/
+    	/*init tables of connection and input-output functions*/
 	resmgr_io_funcs_t io_func;
 	resmgr_connect_funcs_t connect_func;
 	iofunc_func_init(_RESMGR_CONNECT_NFUNCS,&connect_func,_RESMGR_IO_NFUNCS,&io_func);
@@ -38,13 +38,13 @@ int main(int argc, char *argv[])
 		io_func.close_ocb = io_close_ocb;
 		io_func.devctl = io_devctl;
 		io_func.notify = io_notify;		
-    /*for manual (we own not library) open_context_block managment*/
+    	/*for manual (we own not library) open_context_block managment*/
 	iofunc_funcs_t new_func = {_IOFUNC_NFUNCS,new_ocb_calloc,new_ocb_free};
 	iofunc_mount_t new_mount = {0,0,0,0,&new_func};        
         
-    /* init device attr, if you want more than one device attr you must set
-     * RM_DEV_PATH separately and make decision about connect_func and
-     * io_func - they common for all devices or each have unique. */
+    	/* init device attr, if you want more than one device attr you must set
+     	* RM_DEV_PATH separately and make decision about connect_func and
+     	* io_func - they common for all devices or each have unique. */
 	int i;
 	for(i=0;i<RM_MAX_DEV_COUNT;++i)
 	{
@@ -78,9 +78,9 @@ int main(int argc, char *argv[])
 	}
 	fprintf(startlog,"success pulse_attach()\n");
 
-    /*dispatch alloc*/
-    dispatch_context_t *ctp = dispatch_context_alloc(dpp);
-    if( NULL==ctp )
+    	/*dispatch alloc*/
+    	dispatch_context_t *ctp = dispatch_context_alloc(dpp);
+    	if( NULL==ctp )
 	{
 		fprintf(startlog,"fail dispatch_context_alloc(), error = %s\n",strerror(errno));
 		return EXIT_FAILURE;
@@ -96,7 +96,7 @@ int main(int argc, char *argv[])
 	fprintf(startlog,"success Source_StartRoutine()...\n");
 
 	/*infinite dispatcher loop*/
-    while(1)
+    	while(1)
 	{
 		ctp = dispatch_block(ctp);
 		if(NULL==ctp)
